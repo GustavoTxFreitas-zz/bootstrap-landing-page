00419b315e72eaf68125201c0aeee4216916b8de
GustavoTxFreitas <62919596+GustavoTxFreitas@users.noreply.github.com>
2020-09-12 19:36:25 -0300

    Fixing somethings

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,9 @@
-feather.replace()
+var buttonElement = document.querySelector('#email-submit');
+
+buttonElement.onclick = function () {
+    if(email.value) alert('Gustavo Teixeira is open for business.');
+    else return false
+}
+
+// feather icon script
+feather.replace();
