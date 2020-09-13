01391defd892396a07ac9bf647806c8bca527738
GustavoTxFreitas <62919596+GustavoTxFreitas@users.noreply.github.com>
2020-09-13 16:55:03 -0300

    Fixed broken design UI and adjusts on form script.

--- javascript/script.js
+++ javascript/script.js
@@ -1,8 +1,15 @@
+var inputElement = document.querySelector('#email');
 var buttonElement = document.querySelector('#email-submit');
 
-buttonElement.onclick = function () {
-    if(email.value) alert('Gustavo Teixeira is open for business.');
-    else return false
+buttonElement.onclick = function validate() {
+    if(email.value){
+        inputElement.classList.replace('is-invalid', 'is-valid');
+    }
+    else{
+        inputElement.classList.replace('is-valid', 'is-invalid');
+    }
+    alert('Gustavo Teixeira is open for business.');   
+    return false; 
 }
 
 // feather icon script
